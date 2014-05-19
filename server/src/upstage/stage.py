286706bca17739a90ad48ed8b328c50382593953
nikosphillips <mfw4515@aut.ac.nz>
2014-05-19 13:56:26 +1200

    Another potential fix
    Should reset the stage if unassigning media from stage edit page

--- server/src/upstage/stage.py
+++ server/src/upstage/stage.py
@@ -318,7 +318,7 @@ class _Stage(object):
                                    comma separated.
 
     """
-    def save(self,config_file=None):
+    def save(self,config_file=None, callLoad = False):
         """Saves to XML config file"""
         config_file = config_file or self.config_file
         tree = microdom.lmx('stage')
@@ -395,7 +395,8 @@ class _Stage(object):
         nodeOwner.text(self.owner)
         save_xml(tree.node, config_file)
         del tree
-        #self.load() commented out as a potential fix for git#193
+        if(callLoad):
+            self.load()
 
     def wake(self):
         """Set stage to active"""
@@ -572,7 +573,7 @@ class _Stage(object):
                         uploaders_collection[v.key] = v.key
 
         #try to preserve local modification
-
+        callLoad = False
         # PQ & EB added 'self.audios' to this line on 13/10/07 
         # This is for updating the stage xml file from the workshop
         for collection in (self.avatars, self.props, self.backdrops, self.audios):
@@ -585,6 +586,7 @@ class _Stage(object):
                 # list displayed by the selected uploaders and not unassign media not it the current displayed list.
                 elif (k in collection.media) and (k in uploaders_collection):
                     collection.drop_mediafile(k)
+                    callLoad = True
         
         if 'assignToStages' in form:
             log.msg("attempting to assign media to stage nat")
@@ -599,8 +601,8 @@ class _Stage(object):
                 
         if refresh_stage == True:            
             self.soft_reset()
-            
-        self.save()
+        if(callLoad)
+        self.save(callLoad)
     
     """
         Heath Behrens 16/08/2011 - Function added to remove media from this stage object.
