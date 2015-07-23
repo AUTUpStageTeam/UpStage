da0bd2936763d4ad678e373d4c94e103f4316813
Yue <makingitbettergo@gmail.com>
2015-07-23 22:33:18 +1200

    updated demonization process, fixed duplicated files being copied to the wrong path. this fault was found when mtasc compilation process

--- install.py
+++ install.py
@@ -219,17 +219,14 @@ else:
     os.system('mkdir ' + outDir)
     os.system('cp -r ./server/src/* ' + outDir + '/')
     os.system('cp -r ./client/* ' + buildDir + '/')
-    print "Move resources to build directory"
-    os.system('mv '+ buildDir +'/src/image ' + buildDir +'/image')
-    os.system('mv '+ buildDir +'/src/font ' + buildDir +'/font')
     print "Compile resources with mtasc"
     os.system('mtasc -swf simple ' + buildDir + '/src/temp/classes.swf -frame 1 -header 320:200:31 -version 8 -v -strict -msvc -wimp -cp ' + buildDir + '/src/' + buildDir +'/src/App.as')
     print "Compile resources with swfmill"
     os.system('swfmill -v simple ' + buildDir + '/src/application.xml ' + buildDir + '/client.swf')
-    #print "Move compiled client to the predefined canonical directory: " + outDir
-    #os.system('mv ' + buildDir + '/client.swf ' + outDir + '/html/client.swf')
-    #print "Remove the temp build directory"
-    #os.system('rm -rf ' + buildDir)
+    print "Move compiled client to the predefined canonical directory: " + outDir
+    os.system('mv ' + buildDir + '/client.swf ' + outDir + '/html/client.swf')
+    print "Remove the temp build directory"
+    os.system('rm -rf ' + buildDir)
 #	print "Removing .git files from current directory and all subdirectories..."
 #	os.system('rm -rf `find . -type d -name .git`')
 #	copyFiles(appServerDir, True)
